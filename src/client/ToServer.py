2f12bf4ad405670186f9bfee24ba769fcde608f0
Luka Popovici <lukapopovici@users.noreply.github.com>
2024-11-24 17:34:29 +0200

    separated clients and dedicated file

---
+++ src/client/ToServer.py
@@ -0,0 +1,22 @@
+import cv2
+import socket
+import sys
+import random
+import struct
+
+def send_image(file_path, host="127.0.0.1", port=8080):
+    with open(file_path, "rb") as f:
+        img_data = f.read()
+
+    img_size = len(img_data)
+    print(f"Sending image of size: {img_size} bytes.")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+
+        sock.sendall(struct.pack("!I", img_size))  
+
+        sock.sendall(img_data)
+
+        response = sock.recv(1024).decode("utf-8")
+        print(f"Server response: {response}")
---
+++ src/client/nao_robot.py
@@ -0,0 +1,31 @@
+#aces fisier este de copiat in folderul interpretorului python 2.7 
+from ToServer import send_image
+import socket
+import sys
+import random
+import struct
+
+sys.path.append(r'C:\Users\aafdd\Downloads\pynaoqi-python2.7-2.8.6.23-win64-vs2015-20191127_152649\pynaoqi-python2.7-2.8.6.23-win64-vs2015-20191127_152649\lib')
+
+print("Python version:", sys.version)
+
+from naoqi import *
+
+robot_ip = "198.0.0.0"  
+port = random.randint(1024, 65535) 
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5) 
+    sock.connect((robot_ip, port))
+    print("SUCCESE")
+    sock.close()  
+
+except socket.error as e:
+    print("FAIL!")
+
+try:
+    tts = ALProxy("ALTextToSpeech", robot_ip, port)
+    tts.say("Hello!")
+except Exception as e:
+    print("Failed")
