17bfb73a0bf45eb5edc73bef82a5df549b1d1592
Luka Popovici <lukapopovici@users.noreply.github.com>
2024-11-24 17:53:33 +0200

    fixed

--- src/client/ToServer.py
+++ src/client/ToServer.py
@@ -4,19 +4,20 @@ import sys
 import random
 import struct
 
-def send_image(file_path, host="127.0.0.1", port=8080):
-    with open(file_path, "rb") as f:
-        img_data = f.read()
+def send_image(frame, host="127.0.0.1", port=8080):
+    success, encoded_image = cv2.imencode('.jpg', frame)
 
+    if not success:
+        print("Error: Failed to encode image.")
+        return
+
+    img_data = encoded_image.tobytes()  
     img_size = len(img_data)
     print(f"Sending image of size: {img_size} bytes.")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((host, port))
-
-        sock.sendall(struct.pack("!I", img_size))  
-
+        sock.sendall(struct.pack("!I", img_size))
         sock.sendall(img_data)
-
         response = sock.recv(1024).decode("utf-8")
         print(f"Server response: {response}")
--- src/client/client_test.py
+++ src/client/client_test.py
@@ -1,4 +1,25 @@
-from ToServer import send_image
+import socket
+import sys
+import random
+import struct
+
+
+def send_image(file_path, host="127.0.0.1", port=8080):
+    with open(file_path, "rb") as f:
+        img_data = f.read()
+
+    img_size = len(img_data)
+    print(f"Sending image of size: {img_size} bytes.")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+
+        sock.sendall(struct.pack("!I", img_size))  
+
+        sock.sendall(img_data)
+
+        response = sock.recv(1024).decode("utf-8")
+        print(f"Server response: {response}")
 
 if __name__ == "__main__":
     send_image("test.png")
